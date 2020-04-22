1fc2ef7448b5a59b282f146907b5bfdf56661693
mutwiri-2 <devmutwiri2@gmail.com>
2020-04-22 16:22:06 +0300

    create the tic tac toe board

---
+++ tic_tac_toe.py
@@ -0,0 +1,6 @@
+the_board = {'top-l':' ', 'top-m':' ', 'top-r':' ', 'mid-l':' ', 'mid-m':' ', 'mid-r':' ', 'low-l': ' ', 'low-m':' ', 
+'low-r':' '}
+
+import pprint
+pprint.pprint(the_board) # sorted
+print(list(the_board)) # ordered in sequence of entry
